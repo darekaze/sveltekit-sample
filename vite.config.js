4c4073ac238ebd2de6805d46cdf814db14623f93
darekaze <darekaze@gmail.com>
2021-05-19 17:31:34 +0800

    move to use svelte kit

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import svelte from '@sveltejs/vite-plugin-svelte'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), VitePWA()]
-})
